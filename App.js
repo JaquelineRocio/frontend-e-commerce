59e3b7b9c94a433e694ac0afcb2b4f3badd3e73c
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-12-08 23:02:02 -0500

    feat: implement screens

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Toast from "react-native-toast-message";
+
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
+import Auth from "./Context/store/Auth";
+
+import Main from "./Navigators/Main";
+
+import Header from "./Shared/Header";
+
+LogBox.ignoreAllLogs(true);
+
+export default function App() {
+  return (
+    <Auth>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Header />
+          <Main />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
+      </Provider>
+    </Auth>
+  );
+}
